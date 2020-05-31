d6269c1a7590fa31e3e194ef6d058b2e2276af1e
Konstantin Danilyuk <34817112+kdanilyuk@users.noreply.github.com>
2020-05-31 17:27:42 +0300

    Initial commit

---
+++ Editor.Tests/Integration.Tests/Document.Integration.Test.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using AutoMapper;
+using EditorServer.Models;
+using System.Text.Json;
+
+namespace Editor.Tests.Integration.Tests
+{
+    public class DocumentIntegrationTest : IClassFixture<WebApplicationFactory<EditorServer.Startup>>
+    {
+        private readonly WebApplicationFactory<EditorServer.Startup> _factory;
+
+        public DocumentIntegrationTest(WebApplicationFactory<EditorServer.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+
+        public async Task GetAllDocuments()
+        {
+            var client = _factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                BaseAddress = new Uri("http://localhost:5000/"),
+                AllowAutoRedirect = true
+            });
+
+            var serializeOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+
+            var response = await client.GetAsync("api/documents/all");
+            var responseString = response.Content.ReadAsStringAsync();
+            var body = JsonSerializer.Deserialize<IEnumerable<DocumentPreview>>(responseString.Result, serializeOptions);
+
+            Assert.True(true);
+        }
+
+        [Fact]
+
+        public async Task GetDocumentBySubjectIdTest()
+        {
+            var client = _factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                BaseAddress = new Uri("http://localhost:5000/"),
+                AllowAutoRedirect = true
+            });
+
+            var serializeOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+
+            var response = await client.GetAsync("api/documents/get-tree?subjectId=1");
+            var responseString = response.Content.ReadAsStringAsync();
+            var body = JsonSerializer.Deserialize<IEnumerable<DocumentPreviewTree>>(responseString.Result, serializeOptions);
+
+            Assert.True(true);
+        }
+    }
+}
---
+++ EditorServer/Profiles/DocumentProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using EditorServer.Models;
+
+namespace EditorServer.Profiles
+{
+    public class DocumentProfile : Profile
+    {
+        public DocumentProfile()
+        {
+            CreateMap<DocumentDTO, DocumentPreview>();
+            CreateMap<DocumentPreview, DocumentDTO>();
+        }
+    }
+}
